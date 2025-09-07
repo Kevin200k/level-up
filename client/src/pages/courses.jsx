a00ca3e566175ecd8b297cd36c4eb55fcca424b1
Damilola Joshua <damilolajoshua2021@gmail.com>
2025-09-07 21:40:08 +0100

    Installed React-Flow

--- client/src/pages/courses.jsx
+++ client/src/pages/courses.jsx
@@ -48,7 +48,7 @@ const Courses = () => {
                       className="ml-2 p-1 rounded-full hover:bg-gray-200 transition-colors"
                       aria-label="Close"
                     >
-                      <X className="w-5 h-5 text-gray-500 hover:text-gray-700" />
+                      <X className="w-5 h-5 text-gray-500 hover:text-red-500" />
                     </button>
                   </h2>
 
